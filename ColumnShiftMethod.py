d2845e09304376e7cc3ca350d5d77435fb77d360
XIFU SUN <xifu.sun@anu.edu.au>
2021-09-15 17:31:11 +0800

    Add files via upload

--- ColumnShiftMethod.py
+++ ColumnShiftMethod.py
@@ -1,10 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-@author: Xifu Sun
-"""
-
 import numpy as np
 from SALib.sample import sobol_sequence
+import math
 
 def Column_shift(N, k, R, skip_value = None):
     '''
@@ -25,8 +21,14 @@ def Column_shift(N, k, R, skip_value = None):
         Dictionary that contains shifted samples
 
     '''
+    if R > math.factorial(2 * k) - 1:
+        print('The number of replicates exceeds the limit. Possible number of replicates should be less than ' + str (math.factorial(2 * k)))
+        
+        return
+    
     if skip_value == None:
         skip_value = N
+        
     sequence = sobol_sequence.sample(N + skip_value, 2 * k)[skip_value:, :] #generate base sample
     sequence_dict = {} #dict for saving shifted samples
     
@@ -37,22 +39,11 @@ def Column_shift(N, k, R, skip_value = None):
         D_temp = D.copy()
         np.random.shuffle(D_temp)
         
-        while check_dup(D_temp, D_arr, k):
+        while np.any([np.array_equal(D_temp, row) for row in D_arr]):
             np.random.shuffle(D_temp)
         
         D_arr[i, :] = D_temp
         
         sequence_dict[i] = sequence.T[D_temp].T
-        
+
     return sequence_dict
-        
-        
-def check_dup(D_temp, D_arr, k):
-    #Check if Matrix A or Matrix B was already used
-    
-    if np.any([np.array_equal(D_temp[:k], row) for row in D_arr[:, :k]]):
-        return True
-    elif np.any([np.array_equal(D_temp[k:], row) for row in D_arr[:, k:]]):
-        return True
-    
-    return False
